45c73fc2c0d19fbb26142330bb63d3962d98b62c
Valerie Dania <73624288+valeriedania@users.noreply.github.com>
2021-10-05 16:10:42 -0400

    add constructors to class profile

--- Profile.cs
+++ Profile.cs
@@ -7,8 +7,18 @@ namespace DatingProfile
     private string name;
     private int age;
     private string city;
-    private string county;
+    private string country;
     private string pronouns;
     private string[] hobbies;
+
+    public Profile(string name, int age, string city, string country, string pronouns = "they/them")
+    {
+      this.name = name;
+      this.age = age;
+      this.city = city;
+      this.country = country;
+      this.pronouns = pronouns;
+      this.hobbies = new string[0];
+    }
   }
 }
