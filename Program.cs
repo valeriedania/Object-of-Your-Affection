38463713401fe1d315196de337c6111c19b46929
Valerie Dania <73624288+valeriedania@users.noreply.github.com>
2021-10-05 15:55:01 -0400

    instantiate profile object

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace DatingProfile
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Profile sam = new Profile();
+      sam.name = "Sam Drakkila";
+    }
+  }
+}
