1eabd33450b1b4c9bf36c4a7320807713f5d1661
Valerie Dania <73624288+valeriedania@users.noreply.github.com>
2021-10-05 15:52:21 -0400

    add fields to dating profile class

---
+++ Profile.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace DatingProfile
+{ 
+  class Profile
+  {
+    string name;
+    int age;
+    string city;
+    string county;
+    string pronouns;
+    string[] hobbies;
+  }
+}
